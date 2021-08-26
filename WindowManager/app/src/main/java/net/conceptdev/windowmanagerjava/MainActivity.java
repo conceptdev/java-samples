5615036db8d56b32a67c5cfb4fbd3f5b37f3bce9
Craig Dunn <craig.dunn@microsoft.com>
2021-08-26 09:49:54 -0700

    implement Consumer and remove_Listener

--- WindowManager/app/src/main/java/net/conceptdev/windowmanagerjava/MainActivity.java
+++ WindowManager/app/src/main/java/net/conceptdev/windowmanagerjava/MainActivity.java
@@ -3,6 +3,7 @@ package net.conceptdev.windowmanagerjava;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.core.util.Consumer;
 import androidx.window.java.layout.WindowInfoRepositoryCallbackAdapter;
 import androidx.window.layout.DisplayFeature;
 import androidx.window.layout.FoldingFeature;
@@ -30,6 +31,7 @@ import com.google.android.material.internal.ContextUtils;
 
 public class MainActivity extends AppCompatActivity {
     String TAG = "JWM";
+    LayoutStateChangeCallback layoutStateChangeCallback = new LayoutStateChangeCallback();
     WindowInfoRepositoryCallbackAdapter wir;
     ConstraintLayout root;
     TextView outputText;
@@ -57,63 +59,72 @@ public class MainActivity extends AppCompatActivity {
 
         // WindowInfoRepository listener
         Log.d(TAG, "onStart add listener");
-        wir.addWindowLayoutInfoListener(runOnUiThreadExecutor(), (windowLayoutInfo -> {
-
-            // WindowMetrics synchronous call
-            WindowMetrics wm = wmc.computeCurrentWindowMetrics(this);
-            wm = wmc.computeCurrentWindowMetrics(this);
-            String currentMetrics = wm.getBounds().toString();
-            wm = wmc.computeMaximumWindowMetrics(this);
-            String maxMetrics = wm.getBounds().toString();
-            String outMetrics = "\n\n";
-            outMetrics += "Current Window Metrics " + currentMetrics;
-            outMetrics += "\n";
-            outMetrics += "Maximum Window Metrics " + maxMetrics;
-
-            // WindowInfoRepository DisplayFeatures
-            List<DisplayFeature> displayFeatures = windowLayoutInfo.getDisplayFeatures();
-            if (displayFeatures.isEmpty()) {
-                String out = getString(R.string.no_features);
-                out += outMetrics;
-                outputText.setText(out);
-                return;
-            }
-            else {
-                // update screen
-                Log.d(TAG, "window layout contains display feature/s");
-                String finalOutMetrics = outMetrics;
-                displayFeatures.forEach(displayFeature -> {
-                    FoldingFeature foldingFeature = (FoldingFeature)displayFeature;
-                    if (foldingFeature != null)
-                    {   // only set if it's a fold, not other feature type. only works for single-fold devices.
-                        String out = "";
-                        if (foldingFeature.getOrientation() == FoldingFeature.Orientation.HORIZONTAL) {
-                            out += getString(R.string.hinge_is_horizontal);
-                        } else {
-                            out += getString(R.string.hinge_is_vertical);
-                        }
-                        out += "\n";
-                        out += "State is " + foldingFeature.getState().toString();
-                        out += "\n";
-                        out += "OcclusionType is " + foldingFeature.getOcclusionType().toString();
-                        out += "\n";
-                        out += "isSeparating is " + foldingFeature.isSeparating();
-                        out += "\n";
-                        out += "Bounds are " + foldingFeature.getBounds().toString();
-                        out += finalOutMetrics;
-                        outputText.setText(out);
-
-                        return; // only works for one hinge/fold!
-                    }
-                });
-            }
-        }));
+        wir.addWindowLayoutInfoListener(runOnUiThreadExecutor(), layoutStateChangeCallback);
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        //wir.removeWindowLayoutInfoListener(); // TODO?
+        wir.removeWindowLayoutInfoListener(layoutStateChangeCallback);
+    }
+
+    void updateLayout(WindowLayoutInfo windowLayoutInfo)
+    {
+        // WindowMetrics synchronous call
+        WindowMetrics wm = wmc.computeCurrentWindowMetrics(this);
+        wm = wmc.computeCurrentWindowMetrics(this);
+        String currentMetrics = wm.getBounds().toString();
+        wm = wmc.computeMaximumWindowMetrics(this);
+        String maxMetrics = wm.getBounds().toString();
+        String outMetrics = "\n\n";
+        outMetrics += "Current Window Metrics " + currentMetrics;
+        outMetrics += "\n";
+        outMetrics += "Maximum Window Metrics " + maxMetrics;
+
+        // WindowInfoRepository DisplayFeatures
+        List<DisplayFeature> displayFeatures = windowLayoutInfo.getDisplayFeatures();
+        if (displayFeatures.isEmpty()) {
+            String out = getString(R.string.no_features);
+            out += outMetrics;
+            outputText.setText(out);
+            return;
+        }
+        else {
+            // update screen
+            Log.d(TAG, "window layout contains display feature/s");
+            String finalOutMetrics = outMetrics;
+            displayFeatures.forEach(displayFeature -> {
+                FoldingFeature foldingFeature = (FoldingFeature)displayFeature;
+                if (foldingFeature != null)
+                {   // only set if it's a fold, not other feature type. only works for single-fold devices.
+                    String out = "";
+                    if (foldingFeature.getOrientation() == FoldingFeature.Orientation.HORIZONTAL) {
+                        out += getString(R.string.hinge_is_horizontal);
+                    } else {
+                        out += getString(R.string.hinge_is_vertical);
+                    }
+                    out += "\n";
+                    out += "State is " + foldingFeature.getState().toString();
+                    out += "\n";
+                    out += "OcclusionType is " + foldingFeature.getOcclusionType().toString();
+                    out += "\n";
+                    out += "isSeparating is " + foldingFeature.isSeparating();
+                    out += "\n";
+                    out += "Bounds are " + foldingFeature.getBounds().toString();
+                    out += finalOutMetrics;
+                    outputText.setText(out);
+
+                    return; // only works for one hinge/fold!
+                }
+            });
+        }
+    }
+
+    class LayoutStateChangeCallback implements Consumer<WindowLayoutInfo> {
+        @Override
+        public void accept(WindowLayoutInfo windowLayoutInfo) {
+            updateLayout(windowLayoutInfo);
+        }
     }
 
     Executor runOnUiThreadExecutor()
